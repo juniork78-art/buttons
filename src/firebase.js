037a259cf7b3d3e0bb559e68b9d1c58ba4a14c78
juniork78-art <junior.k78@gmail.com>
2026-09-05 17:31:13 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,24 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHcVZFWlTSD8TYV9YCPyQ9mhp_NxzppKM",
-  authDomain: "inspecaopop.firebaseapp.com",
-  projectId: "inspecaopop",
-  storageBucket: "inspecaopop.firebasestorage.app",
-  messagingSenderId: "517844065120",
-  appId: "1:517844065120:web:d7a13da4d4o868bdb9c05",
-  measurementId: "G-2FMV2PZV2S"
+  apiKey: "AIzaSyDmWcZtrfQK2aXDzBdCtcIuPGmHkfpfyrg",
+  authDomain: "botoes-1787c.firebaseapp.com",
+  projectId: "botoes-1787c",
+  storageBucket: "botoes-1787c.firebasestorage.app",
+  messagingSenderId: "233788862144",
+  appId: "1:233788862144:web:9064d961ace070823dc725",
+  measurementId: "G-4MX2B6VX9S"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exportações necessárias para o sistema de login e salvamento de sons
 export const auth = getAuth(app);
 export const db = getFirestore(app);
